cb39c17ca08d1021d9547f750aad402b4790660f
Tok LUO <tokluo@Tok-LUOs-MacBook.local>
2011-09-16 10:51:15 +0100

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f77d392c07e13a8fe25a96ae64006cd985dc11d583c52e6a3bbc79824add9ae0ed3bc9451750665f4f8071ff41dc3fe02f5791414c50fa6abe272c10d0568fea'
